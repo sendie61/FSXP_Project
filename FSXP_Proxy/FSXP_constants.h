abb7d21ff7a977f9626f24cb9d6f932e23db4525
Frits Senders <frits@thuisnet.info>
2015-02-14 13:09:04 +0100

    ok

--- FSXP_Proxy/FSXP_constants.h
+++ FSXP_Proxy/FSXP_constants.h
@@ -13,8 +13,8 @@
  * all supported hardware modules
  */
 enum modules_type {
-	UNKNOWN = 0, DUEPROXY, I2C_IO, SPI_LDISP, I2C_ARD
+	UNKNOWN = 0, DUEPROXY, I2C_IO, SPI_LDISP, I2C_ARD, LAST_MODULE_TYPE
 };
-const char modType[5][11] PROGMEM = {"UNKNOWN\0  ", "DUEPROXY\0 ", "I2C_IO\0   ", "SPI_LDISP\0","I2C_ARD\0  "};
+const char modType[LAST_MODULE_TYPE][11] PROGMEM = {"UNKNOWN\0  ", "DUEPROXY\0 ", "I2C_IO\0   ", "SPI_LDISP\0", "I2C_ARD\0  "};
 
 #endif /* FSXP_CONSTANTS_H_ */
--- FSXP_Proxy/ModuleManager.cpp
+++ FSXP_Proxy/ModuleManager.cpp
@@ -17,19 +17,26 @@ ModuleManager::~ModuleManager() {
 }
 
 bool ModuleManager::processMessage(aJsonObject* msg) {
-	modules_type moduleType= DUEPROXY;
-	ModuleBase* foundModule= NULL;
-	uint8_t subaddr=0;
-	uint16_t arraylen=0;
+
+	ModuleBase* foundModule = NULL;
+	uint8_t subaddr = 0;
+	uint16_t arraylen = 0;
 	bool rv = false;
-	if (msg != NULL) {
-		aJsonObject* modObj = aJson.getObjectItem(msg, modType[moduleType]);
-		if (modObj != NULL) {
-			if ((arraylen = aJson.getArraySize(modObj))!=0){
-				for (uint8_t i = 0; i < arraylen; i++){
-					uint8_t subaddr = aJson.getObjectItem(modObj, "-subaddr")->valueint;
-					if ((foundModule=getModule(moduleType,subaddr))!=NULL){
-						foundModule->processMessage(modObj);
+	for (uint8_t moduleType = UNKNOWN + 1; moduleType < LAST_MODULE_TYPE;
+			moduleType++) {
+		if (msg != NULL) {
+			aJsonObject* modulesObj = aJson.getObjectItem(msg,
+					modType[moduleType]);
+			if (modulesObj != NULL) {
+				if ((arraylen = aJson.getArraySize(modulesObj)) != 0) {
+					for (uint8_t i = 0; i < arraylen; i++) {
+						aJsonObject* modObj = aJson.getArrayItem(modulesObj, i);
+						if (modObj != NULL) {
+							uint8_t subaddr = aJson.getObjectItem(modObj,"-subaddr")->valueint;
+							if ((foundModule = getModule(moduleType, subaddr)) != NULL) {
+								foundModule->processMessage(modObj);
+							}
+						}
 					}
 				}
 			}
